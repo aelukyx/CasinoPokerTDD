d295a6593d5a2896f7dfd90e26eab9fef4df8634
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-11-15 10:44:30 -0500

    Poker.Vs.1.0
    Valida una sola mana para saber que tipo de mano tiene el jugador 1.
    Falta implementar jugador 2 y el ganador. :smiling_imp:

---
+++ CasinoPoker/PokerGame.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CasinoPoker
+{
+    public class PokerGame
+    {
+        public string GetGanadorPoker(string mano1)
+        {
+            string result = "Carta Alta";
+
+            var cartas = new Dictionary<string, int>();
+            cartas.Add("2", 2);
+            cartas.Add("3", 3);
+            cartas.Add("4", 4);
+            cartas.Add("5", 5);
+            cartas.Add("6", 6);
+            cartas.Add("7", 7);
+            cartas.Add("8", 8);
+            cartas.Add("9", 9);
+            cartas.Add("I", 10);
+            cartas.Add("J", 11);
+            cartas.Add("Q", 12);
+            cartas.Add("K", 13);
+            cartas.Add("A", 14);
+
+            List<string> ListCartasMano1 = new List<string>();
+            List<int> ListCartasNumericoTemporal = new List<int>();
+
+            Regex delimitador = new Regex("-");
+            string[] subCarta = delimitador.Split(mano1);
+            foreach (string Cartita in subCarta)
+            {
+                ListCartasMano1.Add(Cartita);
+                ListCartasNumericoTemporal.Add(cartas[Cartita.Substring(0,1)]);
+            }
+
+            //******************** Lista String ************************
+            IEnumerable<string> query = from word in ListCartasMano1
+                                        orderby word.Substring(0, 1) descending //Menor a Mayor
+                                        select word;
+
+            foreach (string str in query)
+            {
+                ListCartasMano1.Insert(0, str);
+            }
+            //**********************************************************
+
+            ListCartasNumericoTemporal.Sort();
+
+            //********************************************************
+
+            result = UnPar(result, cartas, ListCartasMano1);
+
+            result = DosPares(result, cartas, ListCartasMano1);
+
+            result = Trio(result, cartas, ListCartasMano1);
+
+            result = Escalera(result, ListCartasNumericoTemporal);
+
+            result = Color(result, ListCartasMano1, ListCartasNumericoTemporal);
+
+            result = Full(result, cartas, ListCartasMano1);
+
+            result = Poker(result, cartas, ListCartasMano1);
+
+            result = EscaleraColor(result, ListCartasMano1, ListCartasNumericoTemporal);
+
+            result = EscaleraReal(result, ListCartasMano1, ListCartasNumericoTemporal);
+
+            return result;
+
+        }
+
+        private static string EscaleraReal(string result, List<string> ListCartasMano1, List<int> ListCartasNumericoTemporal)
+        {
+            if (ListCartasNumericoTemporal[0] == 10 && ListCartasNumericoTemporal[1] == 11 &&
+                ListCartasNumericoTemporal[2] == 12 && ListCartasNumericoTemporal[3] == 13 &&
+                ListCartasNumericoTemporal[4] == 14 &&
+
+                ListCartasMano1[0].Substring(1, 2) == ListCartasMano1[1].Substring(1, 2) &&
+                ListCartasMano1[1].Substring(1, 2) == ListCartasMano1[2].Substring(1, 2) &&
+                ListCartasMano1[2].Substring(1, 2) == ListCartasMano1[3].Substring(1, 2) &&
+                ListCartasMano1[3].Substring(1, 2) == ListCartasMano1[4].Substring(1, 2))
+            {
+                result = "Escalera Real";
+            }
+            return result;
+        }
+
+        private static string EscaleraColor(string result, List<string> ListCartasMano1, List<int> ListCartasNumericoTemporal)
+        {
+            if (ListCartasNumericoTemporal[0] + 1 == ListCartasNumericoTemporal[1] &&
+                ListCartasNumericoTemporal[1] + 1 == ListCartasNumericoTemporal[2] &&
+                ListCartasNumericoTemporal[2] + 1 == ListCartasNumericoTemporal[3] &&
+                ListCartasNumericoTemporal[3] + 1 == ListCartasNumericoTemporal[4] &&
+
+                ListCartasMano1[0].Substring(1, 2) == ListCartasMano1[1].Substring(1, 2) &&
+                ListCartasMano1[1].Substring(1, 2) == ListCartasMano1[2].Substring(1, 2) &&
+                ListCartasMano1[2].Substring(1, 2) == ListCartasMano1[3].Substring(1, 2) &&
+                ListCartasMano1[3].Substring(1, 2) == ListCartasMano1[4].Substring(1, 2))
+            {
+                result = "Escalera de Color";
+            }
+
+            //Escalera de Color Menor A-2-3-4-5
+            if (ListCartasNumericoTemporal[0] == 2 && ListCartasNumericoTemporal[1] == 3 &&
+                ListCartasNumericoTemporal[2] == 4 && ListCartasNumericoTemporal[3] == 5 &&
+                ListCartasNumericoTemporal[4] == 14 &&
+
+                ListCartasMano1[0].Substring(1, 2) == ListCartasMano1[1].Substring(1, 2) &&
+                ListCartasMano1[1].Substring(1, 2) == ListCartasMano1[2].Substring(1, 2) &&
+                ListCartasMano1[2].Substring(1, 2) == ListCartasMano1[3].Substring(1, 2) &&
+                ListCartasMano1[3].Substring(1, 2) == ListCartasMano1[4].Substring(1, 2))
+            {
+                result = "Escalera de Color";
+            }
+            return result;
+        }
+
+        private static string Poker(string result, Dictionary<string, int> cartas, List<string> ListCartasMano1)
+        {
+            if (cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)] &&
+                cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)] &&
+                cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)])
+            {
+                result = "Poker";
+            }
+
+            if (cartas[ListCartasMano1[4].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)] &&
+                cartas[ListCartasMano1[4].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)] &&
+                cartas[ListCartasMano1[4].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)])
+            {
+                result = "Poker";
+            }
+            return result;
+        }
+
+        private static string Full(string result, Dictionary<string, int> cartas, List<string> ListCartasMano1)
+        {
+            if (cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)] &&
+                cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)] &&
+                cartas[ListCartasMano1[3].Substring(0, 1)] == cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Full";
+            }
+
+            if (cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)] &&
+                 cartas[ListCartasMano1[2].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)] &&
+                 cartas[ListCartasMano1[2].Substring(0, 1)] == cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Full";
+            }
+            return result;
+        }
+
+        private static string Color(string result, List<string> ListCartasMano1, List<int> ListCartasNumericoTemporal)
+        {
+            if (ListCartasMano1[0].Substring(1, 2) == ListCartasMano1[1].Substring(1, 2) &&
+                ListCartasMano1[1].Substring(1, 2) == ListCartasMano1[2].Substring(1, 2) &&
+                ListCartasMano1[2].Substring(1, 2) == ListCartasMano1[3].Substring(1, 2) &&
+                ListCartasMano1[3].Substring(1, 2) == ListCartasMano1[4].Substring(1, 2))
+            {
+                result = "Color";
+            }
+
+            return result;
+        }
+
+        private static string Escalera(string result, List<int> ListCartasNumericoTemporal)
+        {
+            if (ListCartasNumericoTemporal[0] + 1 == ListCartasNumericoTemporal[1] &&
+                ListCartasNumericoTemporal[1] + 1 == ListCartasNumericoTemporal[2] &&
+                ListCartasNumericoTemporal[2] + 1 == ListCartasNumericoTemporal[3] &&
+                ListCartasNumericoTemporal[3] + 1 == ListCartasNumericoTemporal[4])
+            {
+                result = "Escalera";
+            }
+
+            //Escalera A-2-3-4-5
+            if (ListCartasNumericoTemporal[0] == 2 && ListCartasNumericoTemporal[1] == 3 &&
+                ListCartasNumericoTemporal[2] == 4 && ListCartasNumericoTemporal[3] == 5 &&
+                ListCartasNumericoTemporal[4] == 14)
+            {
+                result = "Escalera";
+            }
+            return result;
+        }
+
+        private static string Trio(string result, Dictionary<string, int> cartas, List<string> ListCartasMano1)
+        {
+            if ((cartas[ListCartasMano1[2].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[3].Substring(0, 1)] == cartas[ListCartasMano1[4].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[1].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[1].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)])
+            {
+                result = "Trio";
+            }
+
+
+            if ((cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[1].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[3].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)]
+                    && cartas[ListCartasMano1[3].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)])
+            {
+                result = "Trio";
+            }
+
+            if ((cartas[ListCartasMano1[1].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[2].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)])
+            {
+                result = "Trio";
+            }
+            return result;
+        }
+
+        private static string DosPares(string result, Dictionary<string, int> cartas, List<string> ListCartasMano1)
+        {
+            if ((cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[3].Substring(0, 1)] == cartas[ListCartasMano1[4].Substring(0, 1)]
+
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)])
+            {
+                result = "Dos Pares";
+            }
+
+            if ((cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[2].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)]
+
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)]
+                    && cartas[ListCartasMano1[2].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Dos Pares";
+            }
+
+            if ((cartas[ListCartasMano1[1].Substring(0, 1)] == cartas[ListCartasMano1[2].Substring(0, 1)]) &&
+                 cartas[ListCartasMano1[3].Substring(0, 1)] == cartas[ListCartasMano1[4].Substring(0, 1)]
+
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[1].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Dos Pares";
+            }
+            return result;
+        }
+
+        private static string UnPar(string result, Dictionary<string, int> cartas, List<string> ListCartasMano1)
+        {
+            if ((cartas[ListCartasMano1[0].Substring(0, 1)] == cartas[ListCartasMano1[1].Substring(0, 1)])
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[3].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)]
+
+                    && cartas[ListCartasMano1[2].Substring(0, 1)] != cartas[ListCartasMano1[3].Substring(0, 1)]
+                    && cartas[ListCartasMano1[2].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)]
+                    && cartas[ListCartasMano1[3].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Un Par";
+            }
+
+            if ((cartas[ListCartasMano1[4].Substring(0, 1)] == cartas[ListCartasMano1[3].Substring(0, 1)])
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[0].Substring(0, 1)]
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[1].Substring(0, 1)]
+                    && cartas[ListCartasMano1[4].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+
+
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[1].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[2].Substring(0, 1)]
+                    && cartas[ListCartasMano1[0].Substring(0, 1)] != cartas[ListCartasMano1[4].Substring(0, 1)])
+            {
+                result = "Un Par";
+            }
+            return result;
+        }
+    }
+}
+
+
+
---
+++ JuezPoker/PokerTesting.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using CasinoPoker;
+
+namespace JuezPoker
+{
+    [TestFixture]
+    public class PokerTesting
+    {
+
+        private PokerGame pokerGame { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            pokerGame = new PokerGame();
+        }
+
+        [Test]
+        public void TestCartaAlta()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+
+            // Assert
+            Assert.AreEqual("Carta Alta", result);
+        }
+
+        [Test]
+        public void TestUnPar()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("ARC-ANT-8NP-6RC-2RD");
+
+            // Assert
+            Assert.AreEqual("Un Par", result);
+        }
+
+        [Test]
+        public void TestUnPar1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("4RC-5NT-4NP-8RC-9RD");
+
+            // Assert
+            Assert.AreEqual("Un Par", result);
+        }
+
+        [Test]
+        public void TestUnPar2()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("KRC-7NT-2NP-KRC-JRD");
+
+            // Assert
+            Assert.AreEqual("Un Par", result);
+        }
+
+        [Test]
+        public void TestDosPares()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("4RC-5NT-4NP-8RC-8RD");
+
+            // Assert
+            Assert.AreEqual("Dos Pares", result);
+        }
+
+        [Test]
+        public void TestDosPares1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("3RC-2NT-3NP-5RC-5RD");
+
+            // Assert
+            Assert.AreEqual("Dos Pares", result);
+        }
+
+
+        [Test]
+        public void TestTrio()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("3RC-2NT-3NP-3RC-5RD");
+
+            // Assert
+            Assert.AreEqual("Trio", result);
+        }
+
+        [Test]
+        public void TestEscalera()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("7RC-6NT-9NP-8RC-IRD");
+
+            // Assert
+            Assert.AreEqual("Escalera", result);
+        }
+
+        [Test]
+        public void TestEscalera1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("ARC-INT-JNP-QRC-KRD");
+
+            // Assert
+            Assert.AreEqual("Escalera", result);
+        }
+
+        [Test]
+        public void TestEscalera2()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("5RC-4NT-3NP-2RC-ARD");
+
+            // Assert
+            Assert.AreEqual("Escalera", result);
+        }
+
+        [Test]
+        public void TestEscalera3()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("2RC-4NT-3NP-6RC-5RD");
+
+            // Assert
+            Assert.AreEqual("Escalera", result);
+        }
+
+        [Test]
+        public void TestColor()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("KRC-JNP-INT-6RC-3RD");
+            var result = pokerGame.GetGanadorPoker("2NT-5NT-3NT-6NT-5NT");
+
+            // Assert
+            Assert.AreEqual("Color", result);
+        }
+
+        [Test]
+        public void TestColor1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("4RE-6RE-JRE-ARE-9RE");
+
+            // Assert
+            Assert.AreEqual("Color", result);
+        }
+
+        [Test]
+        public void TestFull()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("3RE-3NE-3RE-ANE-ARE");
+
+            // Assert
+            Assert.AreEqual("Full", result);
+        }
+
+        [Test]
+        public void TestFull1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("QRT-ARP-QRE-ARC-QRC");
+
+            // Assert
+            Assert.AreEqual("Full", result);
+        }
+
+
+        [Test]
+        public void TestPoker()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("ART-8RP-ARE-ARC-ARC");
+
+            // Assert
+            Assert.AreEqual("Poker", result);
+        }
+
+        [Test]
+        public void TestPoker1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("2RT-6RP-2RE-2RC-2RC");
+
+            // Assert
+            Assert.AreEqual("Poker", result);
+        }
+
+        [Test]
+        public void TestEscaleraColor()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("7RD-8RD-9RD-IRD-JRD");
+
+            // Assert
+            Assert.AreEqual("Escalera de Color", result);
+        }
+
+        [Test]
+        public void TestEscaleraColor1()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("ARD-2RD-3RD-5RD-4RD");
+
+            // Assert
+            Assert.AreEqual("Escalera de Color", result);
+        }
+
+        [Test]
+        public void TestEscaleraColor2()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("9NT-QNT-KNT-INT-JNT");
+
+            // Assert
+            Assert.AreEqual("Escalera de Color", result);
+        }
+
+        [Test]
+        public void TestEscaleraReal()
+        {
+            // Act
+            //var result = pokerGame.GetGanadorPoker("4E, 6E, JE, AE, 9E");
+            var result = pokerGame.GetGanadorPoker("ARC-KRC-QRC-IRC-JRC");
+
+            // Assert
+            Assert.AreEqual("Escalera Real", result);
+        }
+
+    }
+}
